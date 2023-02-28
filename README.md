bcc746c7a6ccc3b99c72e96b9e28017057f1e869
Samsul Hadi <samsulhadikediri@gmail.com>
2023-02-28 12:02:07 +0700

    chore: python3

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ Introduction to Computer Vision with TensorFlow
 ```bash
 git clone https://github.com/gablooge/GSP631.git
 cp GSP631/* .
-sudo chmod a+x shell.sh
+
+/usr/bin/python3 -m pip install -U google-cloud-logging --user
+/usr/bin/python3 -m pip install -U pylint --user
+
 ./shell.sh
 ```
 
--- shell.sh
+++ shell.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-python model.py
+python3 model.py
 
 # Explore callbacks
-python callback_model.py
+python3 callback_model.py
 
 # Exercise 1
-python updated_model.py
+python3 updated_model.py
 
 # Exercise 2
 cp exercice_2.py updated_model.py
-python updated_model.py
+python3 updated_model.py
 
 # Exercise 3
 cp exercice_3.py updated_model.py
-python updated_model.py
+python3 updated_model.py
