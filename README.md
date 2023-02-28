edaa020057a8371b140519bc06d0bcb0510c32e0
Samsul Hadi <samsulhadikediri@gmail.com>
2023-02-28 11:49:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# GSP631
+
+Introduction to Computer Vision with TensorFlow
+
+
+# Cloudshell
+```bash
+git clone https://github.com/gablooge/GSP631.git
+cd GSP631/* .
+sudo chmod a+x shell.sh
+./shell.sh
+```
+
---
+++ shell.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python model.py
+
+# Explore callbacks
+python callback_model.py
+
+# Exercise 1
+python updated_model.py
+
+# Exercise 2
+cp exercice_2.py updated_model.py
+python updated_model.py
+
+# Exercise 3
+cp exercice_3.py updated_model.py
+python updated_model.py
