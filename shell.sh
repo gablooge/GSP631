2fe41a95c7be55c09443c9feb5f86b2c5362d3db
Samsul Hadi <samsulhadikediri@gmail.com>
2023-02-28 12:09:48 +0700

    chore: fix command

--- shell.sh
+++ shell.sh
@@ -9,9 +9,7 @@ python3 callback_model.py
 python3 updated_model.py
 
 # Exercise 2
-cp exercice_2.py updated_model.py
-python3 updated_model.py
+python3 exercise_2.py
 
 # Exercise 3
-cp exercice_3.py updated_model.py
-python3 updated_model.py
+python3 exercise_3.py
